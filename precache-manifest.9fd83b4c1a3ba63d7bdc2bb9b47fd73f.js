4c2490f7b3ee100d030a2e8ef4a45ca2855f1cfa
Riellima <marielle.diandy@gmail.com>
2022-04-25 00:02:11 +0200

    Updates

---
+++ precache-manifest.9fd83b4c1a3ba63d7bdc2bb9b47fd73f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85a4f44120d7fbac2849f26fd2e22d13",
+    "url": "%20Riellima%20.github.io/Developpeur-Ethereum-Template/index.html"
+  },
+  {
+    "revision": "3d89e9ca6c7f5eb8652c",
+    "url": "%20Riellima%20.github.io/Developpeur-Ethereum-Template/static/css/main.373c7ea4.chunk.css"
+  },
+  {
+    "revision": "410d15556fa408587b58",
+    "url": "%20Riellima%20.github.io/Developpeur-Ethereum-Template/static/js/2.0cd2c90e.chunk.js"
+  },
+  {
+    "revision": "3d89e9ca6c7f5eb8652c",
+    "url": "%20Riellima%20.github.io/Developpeur-Ethereum-Template/static/js/main.b3d70109.chunk.js"
+  },
+  {
+    "revision": "05666c8321de0781f290",
+    "url": "%20Riellima%20.github.io/Developpeur-Ethereum-Template/static/js/runtime-main.c0b6368b.js"
+  }
+]);
